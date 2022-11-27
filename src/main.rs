42d4cad19cc8b7cde136c522efb1deae39097a22
Anup Jadhav <anup.jadhav@gmail.com>
2022-11-27 06:31:21 +0000

    the tests are working on page 269

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
 use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
