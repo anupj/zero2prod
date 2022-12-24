63d00fc50f37556f2aa633941f66fa84cac3e097
Anup Jadhav <anup.jadhav@gmail.com>
2022-12-24 17:08:58 +0000

    Completed chapter 9

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -98,11 +98,8 @@ async fn get_confirmed_subscribers(
     // Nesting its definition inside the function itself is a simple way
     // to clearly communicate this coupling (and to ensure it doesn't
     // get used elsewhere by mistake).
-    struct Row {
-        email: String,
-    }
-    let rows = sqlx::query_as!(
-        Row,
+
+    let confirmed_subscribers = sqlx::query!(
         r#"
         SELECT email
         FROM subscriptions
@@ -110,15 +107,12 @@ async fn get_confirmed_subscribers(
         "#,
     )
     .fetch_all(pool)
-    .await?;
-
-    // Map into the domain type
-    let confirmed_subscribers = rows
-        .into_iter()
-        .map(|r| match SubscriberEmail::parse(r.email) {
-            Ok(email) => Ok(ConfirmedSubscriber { email }),
-            Err(error) => Err(anyhow::anyhow!(error)),
-        })
-        .collect();
+    .await?
+    .into_iter()
+    .map(|r| match SubscriberEmail::parse(r.email) {
+        Ok(email) => Ok(ConfirmedSubscriber { email }),
+        Err(error) => Err(anyhow::anyhow!(error)),
+    })
+    .collect();
     Ok(confirmed_subscribers)
 }
