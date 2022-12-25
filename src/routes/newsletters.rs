87f7f1594402e6ec0f7c8f171e5924436fbd0214
Anup Jadhav <anup.jadhav@gmail.com>
2022-12-25 11:13:34 +0000

    Everything works upto and including 10.2.3.2

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -10,6 +10,7 @@ use actix_web::ResponseError;
 use anyhow::Context;
 use secrecy::ExposeSecret;
 use secrecy::Secret;
+use sha3::Digest;
 use sqlx::PgPool;
 
 #[derive(thiserror::Error)]
@@ -201,14 +202,18 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    // get sha3_256 bytes encoding of the password
+    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    // Lowercase hexadecimal encoding
+    let password_hash = format!("{:x}", password_hash);
     let user_id: Option<_> = sqlx::query!(
         r#"
         SELECT user_id
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 AND password_hash = $2
         "#,
         credentials.username,
-        credentials.password.expose_secret(),
+        password_hash,
     )
     .fetch_optional(pool)
     .await
